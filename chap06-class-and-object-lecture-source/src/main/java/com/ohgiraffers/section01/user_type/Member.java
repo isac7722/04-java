10a698806dd2d281770d548e1559a4dc6d06a443
isac7722 <isac7722@gmail.com>
2023-10-24 18:19:56 +0900

    23Oct

---
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section01/user_type/Member.java
@@ -0,0 +1,29 @@
+package main.java.com.ohgiraffers.section01.user_type;
+
+public class Member {
+    String id;
+    String pwd;
+    String name;
+    int age;
+    char gender;
+    String[] hobby;
+
+
+
+    public void setInt(int age){
+        if(age<0){
+            System.out.println("나이는 0살보다 작을 수 없어요");
+        }
+        this.age = age;
+    }
+
+    @Override
+    public String toString(){
+
+
+
+        return id + "님이 로그인 하셨습니다. 로그인한 사용자 이름은 "+name+" 나이는: "+age+"성별은 "+gender+"취미는 :"+ hobby[0]+"입니다";
+    }
+
+
+}
